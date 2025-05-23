ca0d873afab100374d1668c494c423699a6212a9
ImJustAPassenger <ferrauto1020@gmail.com>
2025-05-23 16:23:32 +0200

    creation base for the project

---
+++ app/meals/page.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function MealsPage(){
+    return <>
+    <h1>Title</h1>
+        </>
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,5 @@
+export default function MealsSharedPage(){
+    return <>
+    <h1>Title</h1>
+    </>
+}
