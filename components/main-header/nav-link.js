267f1ddc523a1a3971a901dfc06a503b8c2f37f4
ImJustAPassenger <ferrauto1020@gmail.com>
2025-05-24 10:55:21 +0200

    creation component for show meal grid

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -1,14 +1,13 @@
 'use client'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-import classes from "./main-header.module.css";
+import classes from './nav-link.module.css' 
 export default function NavLink({href,children}) {
   const path = usePathname()
     return (
     <Link
       href={href}
-      className={path.startsWith({href}) ? classes.active : undefined}
+      className={path.startsWith(href) ? `${classes.link} ${classes.active}`: classes.link}
     >
         {children}
     </Link>
